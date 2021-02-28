7f989bacb357a05f0b7ae1de4d4112b39bb37d00
Bennet Rupnow <61080947+M1st3rButt3r@users.noreply.github.com>
2021-02-28 16:46:18 +0100

    Update sockethandler.js

--- public/sockethandler.js
+++ public/sockethandler.js
@@ -1,9 +1,11 @@
 const socket = io();
 
+//handles the socket connection to server
 socket.on('connect', () => {
     socket.emit('ready', getCookie("uuid"));
 });
 
+//called when the server emits the "call" event
 socket.on("call", (id, peerid) => {;
     incomingCall(id, peerid);
 });
